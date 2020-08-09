1944ec98a8ba86504fffd1bd7c53739e4bb1e555
sravittxof <travis.fox90@gmail.come>
2020-08-09 01:44:08 -0400

    add bundler, rake, pry open-uri, net/http, JSON, and dotenv to gemspec

---
+++ lib/farmers_markets_nyc.rb
@@ -0,0 +1,6 @@
+require "farmers_markets_nyc/version"
+
+module FarmersMarketsNyc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/farmers_markets_nyc/version.rb
@@ -0,0 +1,3 @@
+module FarmersMarketsNyc
+  VERSION = "0.1.0"
+end
